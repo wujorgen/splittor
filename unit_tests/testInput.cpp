0ccd2db5b34d3588841a1fcb82f40ace48f20b9e
Jorgen Wu <wujorgen@gmail.com>
2025-08-13 23:36:40 -0400

    idk

---
+++ unit_tests/testInput.cpp
@@ -0,0 +1,13 @@
+#include <gtest/gtest.h>
+
+#include <Eigen/Dense>
+
+TEST(Input, Reader) {
+    /*
+    I'm lazy, so the input file specification will just be defined here.
+    There will be a file for scalar inputs, such as domain size, number of grid points, fluid properties, etc.
+    There will be a mask file for 
+    */
+}
+
+TEST(Input, Processor) {}
