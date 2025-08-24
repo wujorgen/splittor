4560489e4d4d6384c51c83a90246b4eb977e63ac
Jorgen Wu <wujorgen@gmail.com>
2025-08-24 01:09:11 -0400

    beginning the move to xtensor, instead of doing any real work

--- src/SolverPoisson.hpp
+++ src/SolverPoisson.hpp
@@ -2,12 +2,16 @@
 #define SOLVER_POISSON
 
 #include <Eigen/Dense>
+#include <xtensor/xarray.hpp>
 
-void SolvePoissonGrid2D(Eigen::ArrayXXd& phi, const Eigen::ArrayXXd& b, const double dx, const double dy, const double etol = 1e-4, const double MAXITERP = 100);
+void SolvePoissonGrid2D(xt::xarray<double>& phi, const xt::xarray<double>& b, const double dx, const double dy, const double ETOLP = 1e-4, const double MAXITERP = 100);
+
+void SolvePoissonGrid2D_Eigen(Eigen::ArrayXXd& phi, const Eigen::ArrayXXd& b, const double dx, const double dy, const double ETOLP = 1e-4, const double MAXITERP = 100);
 
 // void SolvePoissonGrid3D();
 // Eigen does not natively support ArrayXXXd. Would have to either switch to arrayfire, or use a vector of ArrayXXd's.
 // I'm fine with only running 2D stuff for now, so we stick with Eigen.
 // For the unstructured stuff I want to do later, this type of data structure won't matter anyways.
+// 8/24/2025, 1:08AM: The above is a lie we have xtensor now >:)
 
 #endif
--- unit_tests/testSolverPoisson.cpp
+++ unit_tests/testSolverPoisson.cpp
@@ -1,10 +1,53 @@
 #include <gtest/gtest.h>
 
 #include <Eigen/Dense>
+#include <xtensor-blas/xlinalg.hpp>
+#include <xtensor/xarray.hpp>
+#include <xtensor/xview.hpp>
 
 #include "../src/SolverPoisson.hpp"
 
 TEST(SolverPoisson, Grid2D) {
+    int NX = 11;
+    int NY = 11;
+    double XMIN = 0;
+    double XMAX = 2;
+    double YMIN = 0;
+    double YMAX = 2;
+
+    double dx = (XMAX - XMIN) / (NX - 1);
+    double dy = (YMAX - YMIN) / (NY - 1);
+
+    xt::xarray<double> phi = xt::zeros<double>({NX, NY});
+    xt::xarray<double> b = xt::zeros<double>({NX, NY});
+
+    xt::xarray<double> phiCorrect = {
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, -0.290308, -0.580616, -0.379162, -0.210571, -0.10912, -0.051637, -0.0202656, -0.00506552, 0, 0},
+        {0, -0.580616, -1.65304, -0.725462, -0.354053, -0.174271, -0.0771886, -0.02436, 0, 0.00506552, 0},
+        {0, -0.379162, -0.725462, -0.515684, -0.305906, -0.156787, -0.0584867, 0, 0.02436, 0.0202656, 0},
+        {0, -0.210571, -0.354053, -0.305906, -0.197196, -0.0884858, 0, 0.0584867, 0.0771886, 0.051637, 0},
+        {0, -0.10912, -0.174271, -0.156787, -0.0884858, 0, 0.0884858, 0.156787, 0.174271, 0.10912, 0},
+        {0, -0.051637, -0.0771886, -0.0584867, 0, 0.0884858, 0.197196, 0.305906, 0.354053, 0.210571, 0},
+        {0, -0.0202656, -0.02436, 0, 0.0584867, 0.156787, 0.305906, 0.515684, 0.725462, 0.379162, 0},
+        {0, -0.00506552, 0, 0.02436, 0.0771886, 0.174271, 0.354053, 0.725462, 1.65304, 0.580616, 0},
+        {0, 0, 0.00506552, 0.0202656, 0.051637, 0.10912, 0.210571, 0.379162, 0.580616, 0.290308, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}};
+
+    // set source terms
+    b(int(NX / 4), int(NX / 4)) = 100;
+    b(int(3 * NY / 4), int(3 * NY / 4)) = -100;
+
+    SolvePoissonGrid2D(phi, b, dx, dy, 1e-4, 100);
+    for (int i = 0; i < NX; i++) {
+        for (int j = 0; j < NY; j++) {
+            // EXPECT_DOUBLE_EQ(phi(i,j), phiCorrect(i,j));
+            EXPECT_NEAR(phi(i, j), phiCorrect(i, j), 1e-6);
+        }
+    }
+}
+
+TEST(SolverPoisson, Grid2D_Eigen) {
     int NX = 11;
     int NY = 11;
     double XMIN = 0;
@@ -39,7 +82,7 @@ TEST(SolverPoisson, Grid2D) {
     b(int(NX / 4), int(NX / 4)) = 100;
     b(int(3 * NY / 4), int(3 * NY / 4)) = -100;
 
-    SolvePoissonGrid2D(phi, b, dx, dy, 1e-4, 100);
+    SolvePoissonGrid2D_Eigen(phi, b, dx, dy, 1e-4, 100);
     for (int i = 0; i < NX; i++) {
         for (int j = 0; j < NY; j++) {
             // EXPECT_DOUBLE_EQ(phi(i,j), phiCorrect(i,j));
